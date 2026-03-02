98eca23eade88781fc4b36261163c1a619bbd17f
Zynemenix0 <harshveer1119@gmail.com>
2026-03-02 16:42:25 +0530

    Set up LSP servers with default configurations
    Configure LSP servers with default settings and specific Lua settings.

--- lspconfig.lua
+++ lspconfig.lua
@@ -1,1 +1,54 @@
 
+require("nvchad.configs.lspconfig").defaults()
+
+
+
+
+
+-- list of all servers configured.
+lspconfig.servers = {
+    "lua_ls",
+    "clangd",
+ }
+
+-- list of servers configured with default config.
+local default_servers = {"clangd"}
+
+-- lsps with default config
+for _, lsp in ipairs(default_servers) do
+    -- lspconfig[lsp].setup({ -- pre nvim 0.11
+    vim.lsp.config(lsp, { -- nvim 0.11
+        on_attach = on_attach,
+        on_init = on_init,
+        capabilities = capabilities,
+    })
+end
+
+-- lspconfig.lua_ls.setup({ -- pre nvim 0.11
+vim.lsp.config("lua_ls", { -- nvim 0.11
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+
+    settings = {
+        Lua = {
+            diagnostics = {
+                enable = false, -- Disable all diagnostics from lua_ls
+                -- globals = { "vim" },
+            },
+            workspace = {
+                library = {
+                    vim.fn.expand("$VIMRUNTIME/lua"),
+                    vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+                    vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
+                    vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+                    
+                },
+                maxPreload = 100000,
+                preloadFileSize = 10000,
+            },
+        },
+    },
+})
+
+-- read :h vim.lsp.config for changing options of lsp servers 
